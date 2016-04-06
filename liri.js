6f4f0e490621a7649163101d057d398d4bbd0683
ceznik <ceznik@verizon.net>
2016-04-06 18:50:11 -0400

    liri.js with switch statment for functions and working API calls

--- liri.js
+++ liri.js
@@ -1,31 +1,56 @@
 var request = require('request'),
 	twitter = require('twitter'),
-	spotify = require('spotify');
+	spotify = require('spotify'),
+  keys = require('./keys.js');
+
+//console.log(keys.twitterKeys);
+
+switch (process.argv[2]){
+  case 'my-tweets':
+    console.log("my tweets switch");
+    var client = new twitter(keys.twitterKeys);
+    var params = {screen_name: 'HotSpitNYFace'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+      if (!error) {
+        //var json = JSON.parse(tweets);
+        console.log(tweets);
+      }
+    });
+
+    break;
+  case 'spotify-this-song':
+    console.log("spotify switch");
+    //spotify info
+    spotify.search({ type: 'track', query:process.argv[3] }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        console.log(data);
+        // Do something with 'data' 
+    });   
+    break;
+  case 'movie-this':
+    console.log("movie switch");
+    ///movie info
+    request('http://www.omdbapi.com/?t=' + process.argv[3].trim() + '&y=&plot=short&r=json', function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       var json = JSON.parse(body);
+       console.log("imbd rating: "+json.imdbRating) 
+     }
+    })
+    break;
+  case 'do-what-it-says':
+    console.log("do what it says switch");
+    break;
+  default:
+    console.log("Sorry. Command not recognized") ;
+}
 
 
 //twitter info
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
-    console.log(tweets);
-  }
-});
 
-//spotify info
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 
-    // Do something with 'data' 
-});
+// console.log(keys.twitterKeys.consumer_key);
 
-///movie info
 
-request('http://www.omdbapi.com/?t=the+notebook&y=&plot=short&r=json', function (error, response, body) {
- if (!error && response.statusCode == 200) {
-   var json = JSON.parse(boy);
-   console.log("imbd rating: "+json.imdbRating) 
- }
-})
+
